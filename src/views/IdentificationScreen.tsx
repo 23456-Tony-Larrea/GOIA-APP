20e1b683233187dcf8f8e81f216765ca8affd9d8
Anthony Larrea <alarrea@goia.com.ec>
2023-06-16 16:51:25 -0500

    dont send image pleafe fix

--- src/views/IdentificationScreen.tsx
+++ src/views/IdentificationScreen.tsx
@@ -1,50 +1,52 @@
-import React, { useState, useEffect } from 'react';
-import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image } from 'react-native';
+import React, { useState, useEffect, useRef } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, Image, Modal } from 'react-native';
 import Icon from 'react-native-vector-icons/FontAwesome';
 import axios from '../../axios/axios';
-import { Camera } from 'expo-camera';
-import {ICars} from '../Interface/ICars'
+import { ICars } from '../Interface/ICars';
 import jwtDecode from 'jwt-decode';
-import { IDecodedToken } from '../Interface/IDecodedToken';
 import AsyncStorage from '@react-native-async-storage/async-storage';
-import CameraScreem from '../views/CameraScreen'
+import { Camera } from 'expo-camera';
+import { IDecodedToken } from '../Interface/IDecodedToken';
+import {IPhoto} from '../Interface/IPhoto'
 
-const getToken = async () => {
-  try {
-    const token = await AsyncStorage.getItem('token');
-    return token;
-  } catch (error) {
-    console.error('Error al obtener el token:', error);
-    return null;
-  }
-};
-
-const decodeToken = async () => {
-  try {
-    const token = await getToken();
-    if (token) {
-      const decodedToken = jwtDecode<IDecodedToken>(token);
-      console.log('Decoded token:', decodedToken);
-      return decodedToken;
-    } else {
-      console.log('No se encontró un token');
-      return null;
-    }
-  } catch (error) {
-    console.error('Error al decodificar el token:', error);
-    return null;
-  }
-};
 const IdentificationCard = () => {
-  const [idRole,setIdRole]= useState(Number);
+  const [idRole, setIdRole] = useState(Number);
   const [plateNumber, setPlateNumber] = useState('');
   const [vehicleInfo, setVehicleInfo] = useState<ICars[]>([]);
   const [searched, setSearched] = useState(false);
-  const [cameraRef, setCameraRef] = useState<Camera | null>(null);
-  const [hasCameraPermission, setHasCameraPermission] = useState<boolean | null>(null); // Asegura que el estado sea del tipo correcto
-  const [seeSearch,setSeeSearch]= useState(false);
-  const [hasPermission, setHasPermission] = useState<boolean | null>(null);
-  const [photoUri, setPhotoUri] = useState<string | null>(null);
+  const [hasCameraPermission, setHasCameraPermission] = useState<boolean | null>(null);
+  const [seeSearch, setSeeSearch] = useState(false);
+  const [modalVisiblePicture, setModalVisiblePicture] = useState(false);
+  const [capturedPhoto, setCapturedPhoto] = useState<IPhoto | string>('');
+  const [idPicture, setIdPicture]= useState(Number);
+  const cameraRef = useRef<any>(null); // Asegúrate de que el tipo sea correcto
+
+  const getToken = async () => {
+    try {
+      const token = await AsyncStorage.getItem('token');
+      return token;
+    } catch (error) {
+      console.error('Error al obtener el token:', error);
+      return null;
+    }
+  };
+
+  const decodeToken = async () => {
+    try {
+      const token = await getToken();
+      if (token) {
+        const decodedToken = jwtDecode<IDecodedToken>(token);
+        console.log('Decoded token:', decodedToken);
+        return decodedToken;
+      } else {
+        console.log('No se encontró un token');
+        return null;
+      }
+    } catch (error) {
+      console.error('Error al decodificar el token:', error);
+      return null;
+    }
+  };
 
   const fetchRoleId = async () => {
     const decodedToken = await decodeToken();
@@ -53,12 +55,11 @@ const IdentificationCard = () => {
     }
   };
 
-    
   const getPermissions = async () => {
     try {
       const response = await axios.get(`/role_permissions/${idRole}`);
       const permissions = response.data.data;
-      const viewSearch = permissions.find((permission: any) => permission.name ==='insertar usuarios');
+      const viewSearch = permissions.find((permission: any) => permission.name === 'insertar usuarios');
       if (viewSearch && viewSearch.state === false) {
         setSeeSearch(false);
       } else {
@@ -70,16 +71,13 @@ const IdentificationCard = () => {
   };
 
   useEffect(() => {
-        (async () => {
-          const { status } = await Camera.requestCameraPermissionsAsync();
-          setHasCameraPermission(status === 'granted');
-        })();
-       fetchRoleId();
-       getPermissions()
-      },
-      []);
-
-    
+    (async () => {
+      const { status } = await Camera.requestCameraPermissionsAsync();
+      setHasCameraPermission(status === 'granted');
+    })();
+    fetchRoleId();
+    getPermissions();
+  }, []);
 
   const searchVehicle = async () => {
     try {
@@ -92,64 +90,76 @@ const IdentificationCard = () => {
   };
 
   const takePhoto = async (id: number) => {
-    
-    console.log(`Foto tomada del vehículo con ID: ${id}`);
-     
-    if (cameraRef) {
-      const photo = await cameraRef.takePictureAsync();
-      setPhotoUri(photo.uri);
-    }
-    if(`Foto tomada del vehículo con ID: ${id}`){
-        return (
-          <CameraScreem/>
-        )
-    }
-
+    setModalVisiblePicture(true);
+    setIdPicture(id)
   };
-  /* 
-   const takePhoto = async () => {
-    if (cameraRef) {
-      const photo = await cameraRef.takePictureAsync();
-      setPhotoUri(photo.uri);
-    }
-  };
-  */
-  
 
-  if (hasPermission === false) {
-    return <Text>No access to camera</Text>;
+  if (hasCameraPermission === false) {
+    return <Text>No tienes acceso a la cámara</Text>;
   }
- 
-  {hasPermission && (
-    <Camera style={styles.camera} ref={(ref) => setCameraRef(ref)} />
-  )}
+  
+  const takePicture = async (id: number) => {
+    if (cameraRef.current) {
+      const photo: IPhoto = await cameraRef.current.takePictureAsync();
+      setCapturedPhoto(photo.uri);
+      console.log('URI de la foto:', photo.uri);
+  
+      try {
+        const response = await fetch(photo.uri);
+        const blob = await response.blob();
+        console.log('Blob de la foto:', blob);
+  
+        const file = new File([blob], 'photo.jpg', { type: 'image/jpeg' });
+        console.log("eror o no",file)
+  
+      /*   const formData = new FormData();
+        formData.append('document', file);
+        console.log('FormData:', formData);
+  
+        const axiosConfig = {
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        };
+  
+        const axiosResponse = await axios.post(
+          `/takePhoto/${id}/evidence`,
+          formData,
+          axiosConfig
+        );
+  
+        console.log('La foto se ha enviado correctamente');
+        // Realizar cualquier acción adicional con la respuesta del servidor si es necesario
+       */} catch (error) {
+        console.log('Error al enviar la foto:', error);
+      }
+    }
+  };
 
+  
   return (
     <View style={styles.container}>
-      
- <View style={styles.card}>
-    <View >
-      <Text style={styles.cardTitle}>Identificación</Text>
-    </View>
-    <View >
-      <View style={styles.search}>
-        <Icon name="search" size={20} color="#000000" />
-        <TextInput
-          style={styles.searchInput}
-          placeholder="Ingrese el número de placa"
-          placeholderTextColor="#000000"
-          value={plateNumber}
-          onChangeText={setPlateNumber}
-        />
+      <View style={styles.card}>
+        <View>
+          <Text style={styles.cardTitle}>Identificación</Text>
+        </View>
+        <View>
+          <View style={styles.search}>
+            <Icon name="search" size={20} color="#000000" />
+            <TextInput
+              style={styles.searchInput}
+              placeholder="Ingrese el número de placa"
+              placeholderTextColor="#000000"
+              value={plateNumber}
+              onChangeText={setPlateNumber}
+            />
+          </View>
+          <TouchableOpacity style={styles.searchButton} onPress={searchVehicle}>
+            <Text style={styles.searchButtonText}>Buscar</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-      <TouchableOpacity
-        style={styles.searchButton}
-        onPress={searchVehicle}
-      >
-        <Text style={styles.searchButtonText}>Buscar</Text>
-      </TouchableOpacity>
-    </View>
-   </View>
+
       {searched && (
         <>
           {vehicleInfo.length === 0 ? (
@@ -187,26 +197,21 @@ const IdentificationCard = () => {
         </>
       )}
 
+      <Modal visible={modalVisiblePicture} transparent>
+        <Camera style={styles.camera} ref={cameraRef} />
+        <TouchableOpacity style={styles.captureButton} onPress={() => {/* setModalVisiblePicture(false) */
+          takePicture(idPicture)
+        }}>
+          <Text style={styles.captureButtonText}>Tomar foto</Text>
+        </TouchableOpacity>
+      </Modal>
+
       <View style={styles.emptyDiv}></View>
     </View>
   );
 };
 
-export default function App() {
-  return (
-    <View style={styles.appContainer}>
-      <IdentificationCard />
-    </View>
-  );
-}
-
 const styles = StyleSheet.create({
-  appContainer: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    paddingHorizontal: 20,
-  },
   container: {
     width: '100%',
     alignItems: 'center',
@@ -223,56 +228,6 @@ const styles = StyleSheet.create({
     fontSize: 18,
     fontWeight: 'bold',
     marginBottom: 10,
-  },
-  inputContainer: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    borderWidth: 1,
-    borderColor: '#999',
-    borderRadius: 5,
-    paddingHorizontal: 10,
-    marginBottom: 10,
-  },
-  inputIcon: {
-    marginRight: 10,
-  },
-  input: {
-    flex: 1,
-  },
-  searchIcon: {
-    marginLeft: 10,
-  },
-  copyIcon: {
-    marginLeft: 10,
-  },
-  infoContainer: {
-    marginTop: 10,
-  },
-  labelRow: {
-    flexDirection: 'row',
-    marginBottom: 5,
-  },
-  label: {
-    fontWeight: 'bold',
-    marginRight: 5,
-  },
-  value: {
-    flex: 1,
-  },
-  cameraButton: {
-    flexDirection: 'row',
-    backgroundColor: '#FF3B30',
-    borderRadius: 3,
-    padding: 7,
-    alignItems: 'center',
-  },
-  cameraText: {
-    color: '#FFF',
-    marginLeft: 10,
-  },
-  emptyDiv: {
-    height: 200,
-    width: '100%',
   },
   search: {
     flexDirection: 'row',
@@ -292,8 +247,52 @@ const styles = StyleSheet.create({
   searchButtonText: {
     color: '#FFF',
   },
+  cameraButton: {
+    flexDirection: 'row',
+    backgroundColor: '#FF3B30',
+    borderRadius: 3,
+    padding: 7,
+    alignItems: 'center',
+  },
+  cameraText: {
+    color: '#FFF',
+    marginLeft: 10,
+  },
+  infoContainer: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  labelRow: {
+    flexDirection: 'row',
+    marginBottom: 5,
+  },
+  label: {
+    fontWeight: 'bold',
+    marginRight: 5,
+  },
+  value: {
+    flex: 1,
+  },
+  captureButton: {
+    position: 'absolute',
+    bottom: 20,
+    left: '50%',
+    transform: [{ translateX: -50 }],
+    backgroundColor: '#FF3B30',
+    borderRadius: 3,
+    padding: 10,
+  },
+  captureButtonText: {
+    color: '#FFF',
+  },
   camera: {
     flex: 1,
-    width: '100%',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+  },
+  emptyDiv: {
+    marginBottom: 100,
   },
 });
+
+export default IdentificationCard;
