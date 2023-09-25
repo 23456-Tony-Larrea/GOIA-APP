220d97c6331776d6542268ab2e2f64f126e3f9ff
unknown <Administrador@goia.com.ec>
2023-09-25 08:09:12 -0500

    send trmas correctly

--- lib/class/Trama.dart
+++ lib/class/Trama.dart
@@ -35,10 +35,10 @@ class Trama {
     TramaType.MANUALIZ: [36,49,77,49,49,120,35],
     TramaType.SUBIDA: [36,49,77,120,49,83,35],
     TramaType.BAJADA: [36,49,77,120,49,66,35],
-    TramaType.IZQUIERDA: [36, 49, 77, 120, 49, 73, 35],
+    TramaType.IZQUIERDA: [36,49,77,120,49,73,35],
     TramaType.DERECHA: [36,49,77,120,49,68,35],
     TramaType.MANUALDE: [36,49,77,49,50,120,35],
-    TramaType.SUBIDAD: [36, 49, 77, 120, 50, 83, 35],
+    TramaType.SUBIDAD: [36,49,77,120,50,83,35],
     TramaType.BAJADAD: [36,49,77,120,50,66,35],
     TramaType.IZQUIERDAD: [36,49,77,120,50,73,35],
     TramaType.DERECHAD: [36,49,77,120,50,68,35],
@@ -47,18 +47,3 @@ class Trama {
     TramaType.VERTICAL: [36,49,65,86,120,120,35],
   };
 }
-/* 
-  TRAMA_ENCENDIDO = new Uint8Array([36,49,49,49,49,49,35]);
-  TRAMA_APAGADO = new Uint8Array([36,48,48,48,48,48,35]);
-  TRAMA_STOP = new Uint8Array([36,49,120,49,120,84,35]);
-  
-  TRAMA_MANUAL_IZQUIERDA = new Uint8Array([36,49,77,49,49,120,35]);
-  TRAMA_MANUAL_IZQUIERDA_ARRIBA = new Uint8Array([36,49,77,120,49,83,35]);
-  TRAMA_MANUAL_IZQUIERDA_ABAJO = new Uint8Array([36,49,77,120,49,66,35]);
-  TRAMA_MANUAL_IZQUIERDA_IZQUIERDA = new Uint8Array([36,49,77,120,49,73,35]);
-  TRAMA_MANUAL_IZQUIERDA_DERECHA = new Uint8Array([36,49,77,120,49,68,35]);
-
-  TRAMA_MANUAL_DERECHA = new Uint8Array([36,49,77,49,50,120,35]);
-  TRAMA_MANUAL_DERECHA_ARRIBA = new Uint8Array([36,49,77,120,50,83,35]);
- 
- */
